58c9338425436fbb0c5c94c5a3b7e8f0cf0267b2
CynicalCode21 <darrencaldwell00@outlook.com>
2020-11-04 18:48:33 +0000

    Able to access host server maria db, using ssh tunnel

--- actix/src/main.rs
+++ actix/src/main.rs
@@ -1,6 +1,9 @@
 use actix_web::{get, HttpResponse, Responder};
 use chrono::prelude::*;
+use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
+use sqlx::MySqlPool;
+use std::env;
 
 #[derive(Serialize, Deserialize)]
 struct MyText {
@@ -19,6 +22,18 @@ async fn get_text() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
+    dotenv().unwrap();
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL").unwrap()).await;
+    let pool = match pool {
+        Ok(pool) => pool,
+        Err(error) => return Err(std::io::Error::new(std::io::ErrorKind::Other, error)),
+    };
+    let result = sqlx::query!("SELECT * FROM users")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+    println!("{:?}", result);
+    println!("{}", result.user_id);
 
     // start server with service get_text to process /text Gets
     HttpServer::new(|| App::new().service(get_text))
