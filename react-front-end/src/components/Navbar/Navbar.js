1d6a78882a4467e97525ddbea9dd6f8001382f8e
Raiyan <raiyan.c15@gmail.com>
2020-11-08 21:16:21 +0000

    added posts to nav bar

--- react-front-end/src/components/Navbar/Navbar.js
+++ react-front-end/src/components/Navbar/Navbar.js
@@ -11,11 +11,20 @@ class Navbar extends React.Component {
             <nav>
                 <h1 className="navbar-logo">CS3099</h1>
                 <ul className="nav-links">
+                    <Link to="/">
+                        <li>Home</li>
+                    </Link>
                     <Link to="/login">
                         <li>Login</li>
                     </Link>
                     <Link to="/register">
                         <li>Register</li>
+                    </Link>
+                    <Link to="/posts">
+                        <li>Posts</li>
+                    </Link>
+                    <Link to="/post">
+                        <li>Post</li>
                     </Link>
                 </ul>
             </nav>
