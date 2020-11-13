645ea87de711bfbdb4dd8b941a7b082cdf429c3b
Euan Lacy <euan.bateslacy@protonmail.com>
2020-11-13 17:11:23 +0000

    feat: auth post comment

--- actix/src/comments/routes.rs
+++ actix/src/comments/routes.rs
@@ -1,6 +1,8 @@
 use super::comments;
 use actix_web::{web, get, post, HttpResponse, Responder};
+use actix_web_httpauth::extractors::bearer::BearerAuth;
 use sqlx::MySqlPool;
+use crate::auth::decode_jwt;
 
 #[get("/api/comments/{id}")]
 async fn get_comment(web::Path(id): web::Path<u64>, pool: web::Data<MySqlPool>) -> impl Responder {
@@ -19,14 +21,23 @@ async fn get_comments(web::Path(id): web::Path<u64>, pool: web::Data<MySqlPool>)
 }
 
 #[post("/api/posts/{id}/comments")]
-async fn post_comment(web::Path(id): web::Path<u64>, pool: web::Data<MySqlPool>,
-                      comment: web::Json::<comments::CommentRequest>) -> impl Responder {
-    println!("here");
-    match comments::insert_comment(id, comment.into_inner(), &pool).await {
-        Ok(comments) => HttpResponse::Ok().json(comments),
-        Err(_) => HttpResponse::InternalServerError().body(""),
+async fn post_comment(web::Path(id): web::Path<u64>,
+                      pool: web::Data<MySqlPool>,
+                      auth: BearerAuth,
+                      comment: web::Json::<comments::CommentRequest>
+) -> impl Responder {
+    if let Ok(user_id) = decode_jwt(auth.token()) {
+        if user_id == comment.user_id {
+            return match comments::insert_comment(id, comment.into_inner(), &pool).await {
+                Ok(comments) => HttpResponse::Ok().json(comments),
+                Err(_) => HttpResponse::InternalServerError().body(""),
+            };
+        }
     }
+
+    HttpResponse::Forbidden().body("Invalid token")
 }
+
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(get_comment);
     cfg.service(get_comments);
