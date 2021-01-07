370de6f74298c6a9da26e79195c6cbb8a378f4e3
cs3099user-b5 <cs3099user-b5@trenco.cs.st-andrews.ac.uk>
2021-01-07 13:38:41 +0000

    enabled CORS for any resource, needs better auth.

--- actix/src/main.rs
+++ actix/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{App, HttpServer, middleware::Logger};
+use actix_cors::Cors;
 use anyhow::Result;
 use dotenv::dotenv;
 use env_logger::Env;
@@ -16,6 +17,13 @@ async fn main() -> Result<()> {
     dotenv().unwrap(); // update env with .env file.
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let pool = MySqlPool::connect(&env::var("DATABASE_URL").unwrap()).await?;
+
+    let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["GET", "POST"])
+            .allow_any_header()
+            .supports_credentials()
+            .max_age(3600);
 
     // start server with service get_text to process /text Gets
     HttpServer::new(move || {
