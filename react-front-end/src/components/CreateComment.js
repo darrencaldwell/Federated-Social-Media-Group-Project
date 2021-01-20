8319162f37dd3574cd19cb8678fdb84eb5a95bd6
dean <dck4@st-andrews.ac.uk>
2021-01-20 19:30:43 +0000

    cleaned, separated and bootstrapped

--- react-front-end/src/components/CreateComment.js
+++ react-front-end/src/components/CreateComment.js
@@ -1,14 +1,76 @@
-import React from "react";
-import {Button, Container} from "react-bootstrap";
+import React from 'react';
+import {Button, Container, Form, FormGroup} from 'react-bootstrap'
+import '../styling/create-post.css'
 
-class CreateComment extends React.Component {
+//props: url
+class Make extends React.Component {
+    constructor(props) {
+        super(props);
+        this.changeTitle = this.changeTitle.bind(this);
+        this.changeBody = this.changeBody.bind(this);
+        const defaultBody = 'Put the body of your comment here';
+        const defaultTitle = 'Title';
+        this.state = {
+            buttonText: 'Create Comment',
+            defaultTitle: defaultTitle,
+            titleText: defaultTitle,
+            defaultBody: defaultBody,
+            bodyText: defaultBody,
+        };
+    }
+
+    submit() {
+        // if no text has been entered, it will return to default before the button is pressed
+        // don't worry about title if in comment mode
+        if ((this.state.titleText === this.state.defaultTitle && !this.mode) ||
+            this.state.bodyText === this.state.defaultBody) {
+            alert('Please enter a title and body');
+        } else {
+            fetch(this.props.url, {
+                method: "POST",
+                withCredentials: true,
+                credentials: 'include',
+                headers: {
+                    'Authorization': "Bearer " + localStorage.getItem('token'),
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        "commentContent": this.state.bodyText,
+                        "userId": localStorage.getItem('userId'),
+                        "username": localStorage.getItem('username')
+                    }
+                )
+            }).then(responseJson => {
+                console.log(responseJson);
+            }).catch(error => this.setState({
+                message: "Error posting post: " + error
+            }));
+        }
+    }
+
+    changeTitle(v) {
+        this.setState({titleText: v.target.value})
+    }
+
+    changeBody(v) {
+        this.setState({bodyText: v.target.value})
+    }
+
     render() {
         return (
-            <Container className="mt-3">
-                <Button variant="light">Create Comment</Button>
+            <Container>
+                <Form className="createPost">
+                    <Form.Label>Create Post</Form.Label>
+                    <FormGroup controlId="create-title">
+                        <Form.Control onChange={this.changeTitle} type="text" placeholder={this.state.defaultTitle}/>
+                        <Form.Control onChange={this.changeBody} as="textarea" rows={3} placeholder={this.state.defaultBody}/>
+                    </FormGroup>
+                    <Button variant="light" onClick={() => this.submit()}>{this.state.buttonText}</Button>
+                </Form>
             </Container>
-        )
+        );
     }
 }
 
-export default CreateComment
+export default Make
--- react-front-end/src/pages/CreatePost.js
+++ react-front-end/src/pages/CreatePost.js
@@ -2,98 +2,16 @@ import React from 'react';
 import {Button, Container, Form, FormGroup} from 'react-bootstrap'
 import '../styling/create-post.css'
 
-class Title extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.props.onChange(e.target.value);
-    }
-
-    handleBlur() {
-        if (this.props.value === '') {
-            this.props.onChange(this.props.default);
-        }
-    }
-
-    handleFocus() {
-        if (this.props.value === this.props.default) {
-            this.props.onChange('');
-        }
-    }
-
-    render() {
-        const value = this.props.value;
-        return (
-            <div>
-                <input
-                    value={value}
-                    onChange={this.handleChange}
-                    onFocus={() => this.handleFocus()}
-                    onBlur={() => this.handleBlur()}/>
-            </div>
-        );
-    }
-}
-
-class Body extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.props.onChange(e.target.value);
-    }
-
-    handleBlur() {
-        if (this.props.value === '') {
-            this.props.onChange(this.props.default);
-        }
-    }
-
-    handleFocus() {
-        if (this.props.value === this.props.default) {
-            this.props.onChange('');
-        }
-    }
-
-    render() {
-        return (
-            <div>
-    <textarea
-        className="text" value={this.props.value} onChange={this.handleChange}
-        onFocus={() => this.handleFocus()}
-        onBlur={() => this.handleBlur()}>
-    </textarea>
-            </div>
-        );
-    }
-}
-
-function SendButton(props) {
-    return (
-        <button
-            className="sendButton"
-            onClick={(props.onClick)}>
-            {props.value}
-        </button>
-    );
-}
-
-//props: url, mode
+//props: url
 class Make extends React.Component {
     constructor(props) {
         super(props);
         this.changeTitle = this.changeTitle.bind(this);
         this.changeBody = this.changeBody.bind(this);
-        this.mode = this.props.mode === "comment";
-        const defaultBody = this.mode ? 'Put the body of your comment here' : 'Put the body of your post here';
+        const defaultBody = 'Put the body of your post here';
         const defaultTitle = 'Title';
         this.state = {
-            buttonText: this.mode ? 'Create Comment' : 'Create Post',
+            buttonText: 'Create Post',
             defaultTitle: defaultTitle,
             titleText: defaultTitle,
             defaultBody: defaultBody,
@@ -116,22 +34,13 @@ class Make extends React.Component {
                     'Authorization': "Bearer " + localStorage.getItem('token'),
                     'Content-Type': 'application/json'
                 },
-                body: this.mode ? JSON.stringify(
-                    {
-                        "commentContent": this.state.bodyText,
-                        "userId": localStorage.getItem('userId'),
-                        "username": localStorage.getItem('username')
-                    }
-                ) : JSON.stringify({
+                body: JSON.stringify({
                     "postTitle": this.state.titleText,
                     "postContents": this.state.bodyText,
                     "userId": localStorage.getItem('userId')
                 })
             }).then(responseJson => {
                 console.log(responseJson);
-                console.log(this.state.titleText);
-                console.log(this.state.bodyText);
-                console.log(localStorage.getItem('userId'));
             }).catch(error => this.setState({
                 message: "Error posting post: " + error
             }));
@@ -139,71 +48,25 @@ class Make extends React.Component {
     }
 
     changeTitle(v) {
-        this.setState({titleText: v})
+        this.setState({titleText: v.target.value})
     }
 
     changeBody(v) {
-        this.setState({bodyText: v})
-    }
-
-    renderTitle() {
-        return (this.mode ? null :
-                <Title
-                    value={this.state.titleText}
-                    default={this.state.defaultTitle}
-                    onChange={this.changeTitle}
-                />
-        );
-    }
-
-    renderText() {
-        return (
-            <Body
-                value={this.state.bodyText}
-                default={this.state.defaultBody}
-                onChange={this.changeBody}
-            />
-        );
-    }
-
-    renderButton() {
-        return (
-            <SendButton
-                value={this.state.buttonText}
-                onClick={() => this.submit()}
-            />
-        );
+        this.setState({bodyText: v.target.value})
     }
 
     render() {
         return (
-            // <Container>
-            //     <Form className="createPost">
-            //         <Form.Label>Create Post</Form.Label>
-            //         <FormGroup controlId="create-title">
-            //             <Form.Control type="text" placeholder="Type title here.."/>
-            //             <Form.Control as="textarea" rows={3} placeholder="Type main body of text here.."/>
-            //         </FormGroup>
-            //         <Button variant="light" onClick={() => this.submit()}>Create</Button>
-            //     </Form>
-            // </Container>
-            <form>
-                <h3>Make Posts</h3>
-                <div>
-                    <div className="title">
-                        {this.renderTitle()}
-                    </div>
-            
-                    <div className="text">
-                        {this.renderText()}
-                    </div>
-            
-                    <div className="sendButton">
-                        {this.renderButton()}
-                    </div>
-            
-                </div>
-            </form>
+            <Container>
+                <Form className="createPost">
+                    <Form.Label>Create Post</Form.Label>
+                    <FormGroup controlId="create-title">
+                        <Form.Control onChange={this.changeTitle} type="text" placeholder={this.state.defaultTitle}/>
+                        <Form.Control onChange={this.changeBody} as="textarea" rows={3} placeholder={this.state.defaultBody}/>
+                    </FormGroup>
+                    <Button variant="light" onClick={() => this.submit()}>{this.state.buttonText}</Button>
+                </Form>
+            </Container>
         );
     }
 }
