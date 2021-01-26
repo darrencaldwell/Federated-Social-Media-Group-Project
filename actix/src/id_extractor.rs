81459b61bc22730a1e0e08f189e73b007cc1607e
Euan Lacy <euan.bateslacy@protonmail.com>
2021-01-26 10:50:30 +0000

    feat: add extractor for getting user_id from header

---
+++ actix/src/id_extractor.rs
@@ -0,0 +1,22 @@
+use serde::Deserialize;
+use actix_web::{FromRequest, HttpRequest, dev, error::ErrorUnauthorized};
+use futures_util::future::{err, ok, Ready};
+
+#[derive(Debug, Deserialize)]
+pub struct UserId {
+    id: String
+}
+
+impl FromRequest for UserId {
+    type Error = actix_web::Error;
+    type Future = Ready<Result<Self, Self::Error>>;
+    type Config = ();
+
+    fn from_request(req: &HttpRequest, payload: &mut dev::Payload) -> Self::Future {
+        match req.headers().get("user_id") {
+            Some(user_id) => ok(UserId{id: user_id.to_str().unwrap().to_string()}),
+            None => err(ErrorUnauthorized("No user_id in header")),
+        }
+        
+    }
+}
