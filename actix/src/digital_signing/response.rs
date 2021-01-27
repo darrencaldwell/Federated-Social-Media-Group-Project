2c17a3a15f1b3d25995f245958c81115025090eb
CynicalCode21 <darrencaldwell00@outlook.com>
2021-01-27 14:30:57 +0000

    wip: working, needs more robust error handling

--- actix/src/digital_signing/response.rs
+++ actix/src/digital_signing/response.rs
@@ -59,15 +59,12 @@ where
     }
 
     fn call(&mut self, req: ServiceRequest) -> Self::Future {
-        println!("oo");
         let fut = self.service.call(req);
 
         Box::pin(async move {
             let mut res = fut.await?;
-            println!("aa");
 
             // create signature-input header
-
             // need sig1=(x, y, z); keyId=x
             let header_string = "sig1=(*request-target, host); keyId=https://cs3099user-b5.host.cs.st-andrews.ac.uk/api/key; alg=hs2019";
             res.headers_mut().insert(HeaderName::from_static("signature-input"), HeaderValue::from_static(header_string));
@@ -76,13 +73,8 @@ where
             // TODO: check if we want to not just create our own host header, instead of using the
             // others host
             let string_to_sign = format!("*request-target: {}\nhost: {}", res.request().path(), res.request().headers().get("host").unwrap().to_str().unwrap());
-               println!("response: {}", string_to_sign);
 
-            println!("yeet");
             let key_pair = res.request().app_data::<Data<PKey<Private>>>().unwrap().clone();
-            println!("yeet");
-            let public_key_pem = key_pair.public_key_to_pem().unwrap();
-            println!("{:?}", std::str::from_utf8(&public_key_pem));
 
             let mut signer = Signer::new(MessageDigest::sha512(), &key_pair).unwrap();
             signer.set_rsa_padding(Padding::PKCS1_PSS).unwrap();
@@ -92,7 +84,6 @@ where
 
             res.headers_mut().insert(HeaderName::from_static("signature"), HeaderValue::from_str(&enc_signature).unwrap());
 
-            println!("Hi from response: {:?}", res.headers());
             Ok(res)
         })
     }
