d7684c84ad6d647375f6c8e94f9682e7c4ef8505
CynicalCode21 <darrencaldwell00@outlook.com>
2021-03-17 15:30:37 +0000

    fix: attempt 2 better deployment

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd backend
 sh sshTunnel.sh
-nolimit cargo build --release
 pkill -x actix
 cargo run --release >> log.log 2>&1
--- tmux.sh
+++ tmux.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sh deploy_frontend.sh &
+
+cd backend
+nolimit cargo build --release
+cd ../
+
 session="deploy"
 tmux has-session -t $session 2>/dev/null
 
@@ -6,6 +12,4 @@ if [ $? = 0 ]; then
 	# kill if exists
 	tmux kill-session -t $session
 fi
-
-./deploy_frontend.sh &
 tmux new-session -d -s 'deploy' './deploy_backend.sh; exec $SHELL'
