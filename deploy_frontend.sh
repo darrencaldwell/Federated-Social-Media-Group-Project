afb7cd86b9f996833573ed5a6a5a88b1a7b76716
cs3099user-b5 <cs3099user-b5@trenco.cs.st-andrews.ac.uk>
2021-03-17 17:23:15 +0000

    feat: working less downtime CI/CD

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd frontend
-npm ci
-npm run build
+nolimit npm ci
+nolimit npm run build
 rm -rf /cs/home/cs3099user-b5/nginx_defualt/build/
 cp -r build/* /cs/home/cs3099user-b5/nginx_default/
