f3bce2de06c7fe51a145f11ecb7aeb78e1b2cfef
cs3099user-b5 <cs3099user-b5@trenco.cs.st-andrews.ac.uk>
2021-03-17 16:45:34 +0000

    fix: should exit processes correctly now

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 cd backend
-pid_ssh=$(pgrep -f 'ssh.*-f')
-pid_actix=$(pidof actix)
-while kill -0 $pid_ssh; do
-	sleep 1
-done
-while kill -0 $pid_actix; do
-	sleep 1
-done
+pkill -x actix
+ps -lef | grep ssh | grep 21463 | awk "{print \$4}" | xargs kill
 sh sshTunnel.sh
+echo "Starting..."
 cargo run --release >> log.log 2>&1
