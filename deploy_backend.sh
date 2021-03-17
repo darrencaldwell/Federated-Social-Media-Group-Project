5a4a3fca158ed380580f111b8330f3234efe46ab
CynicalCode21 <darrencaldwell00@outlook.com>
2021-03-17 16:16:52 +0000

    feat: still trying CI

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd backend
+pid_ssh=$(pgrep -f 'ssh.*-f')
+pid_actix=$(pidof actix)
+while kill -0 $pid_ssh; do
+	sleep 1
+done
+while kill -0 $pid_actix; do
+	sleep 1
+done
 sh sshTunnel.sh
-pkill -x actix
 cargo run --release >> log.log 2>&1
--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -2,5 +2,5 @@
 cd frontend
 npm ci
 npm run build
-rm -rf /cs/home/cs3099user-b5/nginx_defualt/build
-cp -r build/* /cs/home/cs3099user-b5/nginx_defualt/
+rm -rf /cs/home/cs3099user-b5/nginx_defualt/build/
+cp -r build/* /cs/home/cs3099user-b5/nginx_default/
