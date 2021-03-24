f9431c80557f67a735b58e439e77304e6d6bb536
Raiyan <raiyan.c15@gmail.com>
2021-03-24 10:17:27 +0000

    allows moving from post to account and account to forum

--- frontend/src/components/PostPreview.js
+++ frontend/src/components/PostPreview.js
@@ -10,20 +10,21 @@ export class PostPreview extends Component {
             <Card border="dark" className="mt-3" >
                 <Card.Body>
                     <div class="post-preview-container">
-                        <Voting class="voting-post" upvotes={this.props.post.upvotes} 
-                            downvotes={this.props.post.downvotes} 
-                            _userVotes={this.props.post._userVotes}
-                            type="posts"
-                            postID={this.props.post.id}
-                            impID={this.props.impID}
-                        ></Voting>
-                        {/*Links to the post itself, to view/make comments. Removing the /api part directs you to the correct app page. */}
-                        <div class="post">
-                            <Card.Link href={'/' + this.props.impID + '/' + this.props.forumID + '/' + this.props.subforumID + '/' + this.props.post.id}>{this.props.post.postTitle}</Card.Link>
-                            <Card.Subtitle className="text-muted">
-                            by: {this.props.post.username} on TIME
-                            </Card.Subtitle>
-                        </div>
+                    <Voting class="voting-post" upvotes={this.props.post.upvotes}
+    downvotes={this.props.post.downvotes}
+    _userVotes={this.props.post._userVotes}
+    type="posts"
+    postID={this.props.post.id}
+    impID={this.props.impID}
+    />
+                    {/*Links to the post itself, to view/make comments. Removing the /api part directs you to the correct app page. */}
+                    <div class="post">
+                    <Card.Link href={'/' + this.props.impID + '/' + this.props.forumID + '/' + this.props.subforumID + '/' + this.props.post.id}>{this.props.post.postTitle}</Card.Link>
+                    <Card.Subtitle className="text-muted">
+                        Post made by: <Card.Link href={'/account'}>{this.props.post.username} USERNAME_HERE</Card.Link> on TIME
+                    </Card.Subtitle>
+                    <Card.Text>{this.props.post.postContents}</Card.Text>     {/*Use the body from the prop as the body */}
+                    </div>
                     </div>
                     <Card.Body>
                      <Card.Text>{this.props.post.postContents}</Card.Text>     {/*Use the body from the prop as the body */}
