72bc2bb80b738745950c9afb03e1a273a9ad30a6
cs3099user-b5 <cs3099user-b5@trenco.cs.st-andrews.ac.uk>
2021-04-01 14:36:00 +0100

    change: only log latest deploy

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -4,4 +4,4 @@ pkill -x actix
 ps -lef | grep ssh | grep 21463 | awk "{print \$4}" | xargs kill
 sh sshTunnel.sh
 echo "Starting..."
-cargo run --release >> log.log 2>&1
+cargo run --release > log.log 2>&1
