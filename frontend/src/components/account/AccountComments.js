437ddab0f2b33a37c3e91d25bad75e5dadf6b054
dean <dck4@st-andrews.ac.uk>
2021-04-08 19:20:19 +0100

    correct comment display in usercomments

--- frontend/src/components/account/AccountComments.js
+++ frontend/src/components/account/AccountComments.js
@@ -1,8 +1,23 @@
-import React from "react";
+import {React, Component} from 'react';
 import axios from "axios";
-import {Card, Container} from "react-bootstrap";
+import {Card, Container, Button, ButtonGroup} from "react-bootstrap";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Avatar, {Cache} from 'react-avatar';
+import Voting from '../Voting'
+import TimeSince from '../TimeSince';
+import '../../styling/container-pages.css';
 
-class AccountComments extends React.Component {
+// for react avatar
+const cache = new Cache({
+
+    // Keep cached source failures for up to 7 days
+    sourceTTL: 7 * 24 * 3600 * 1000,
+
+    // Keep a maximum of 0 entries in the source cache (we don't care about remembering broken links!)
+    sourceSize: 0
+});
+
+class AccountComments extends Component {
 
     constructor(props) {
         super(props);
@@ -38,24 +53,76 @@ class AccountComments extends React.Component {
             return (
                 <Container>
                     <Card.Title>Your Comments</Card.Title>
+                    {/* this is just a crude copy-paste, but it should work. Ideally this should use the Comments component. */}
                     {this.state.commentList.map((comment) => (
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Comment by {comment.username}</Card.Title>
-                                <Card.Subtitle>Comment Id: {comment.id} Post Id: {comment.postId}</Card.Subtitle>
-                                <Card.Text>{comment.commentContent}</Card.Text>
-                                {/*{console.log('PATH TO COMMENTS',start_url +*/}
-                                {/*    JSON.stringify(parseInt(post._links.forum.href.split("/").pop())) + '/' +*/}
-                                {/*    JSON.stringify(parseInt(post._links.subforum.href.split("/").pop())) + '/' +*/}
-                                {/*    JSON.stringify(parseInt(post._links.self.href.split("/").pop()*/}
-                                {/*    )))}*/}
-                                {/*<Card.Link href={start_url +*/}
-                                {/*JSON.stringify(parseInt(post._links.forum.href.split("/").pop())) + '/' +*/}
-                                {/*JSON.stringify(parseInt(post._links.subforum.href.split("/").pop())) + '/' +*/}
-                                {/*JSON.stringify(parseInt(post._links.self.href.split("/").pop()*/}
-                                {/*))}>Take me to the post {post.postId}</Card.Link>*/}
-                            </Card.Body>
-                        </Card>
+                        <Card border="dark small-separator">
+                        <Card.Body>
+                            <div className="comment-columns">
+                                <div className="post-comment-voting-container">
+                                    <Voting className="voting-post"
+                                            upvotes={comment.upvotes}
+                                            downvotes={comment.downvotes}
+                                            _userVotes={comment._userVotes}
+                                            type="comments"
+                                            postID={comment.id}
+                                            impID="1"
+                                    ></Voting>
+                                    <div className="voting-adj">
+                                        <CardActionArea href={'/user/' + btoa(comment._links.user.href)}>
+                                            <Avatar cache={cache} size="50" round={true} src={this.state.profilePicture}
+                                                name={comment.username}/>
+                                            {"  "} {comment.username}
+                                        </CardActionArea>
+                                        <Card.Subtitle className="text-muted mt-1 time-since">
+                                            <TimeSince createdTime={comment.createdTime}/>
+                                        </Card.Subtitle>
+                                        <Card.Subtitle className="text-muted mt-1 time-since">
+                                            <TimeSince createdTime={comment.createdTime}
+                                                       modifiedTime={comment.modifiedTime}/>
+                                        </Card.Subtitle>
+                                    </div>
+                                </div>
+                                <Card.Text className="mt-3 comment-body">{comment.commentContent}</Card.Text>
+                                <ButtonGroup vertical className="buttons">
+                                    <ButtonGroup>        
+                                        <Button className="button edit-button"
+                                           href={"/1" + JSON.stringify(parseInt(comment._links.forum.href.split("/").pop())) + 
+                                                  "/" + JSON.stringify(parseInt(comment._links.subforum.href.split("/").pop())) +
+                                                  "/" + comment.postId + 
+                                                  "/" + comment.id + "/edit"}>🖉</Button>
+                                        <Button className='button delete-button' onClick={() => {
+                                            if (window.confirm('Are you sure you wish to delete this comment?\n THIS CANNOT BE UNDONE!')) this.delete()}}
+                                           href={"."}>🗑</Button>
+                                    </ButtonGroup>
+                                    <a className="button reply-button"
+                                       href={"/1" + JSON.stringify(parseInt(comment._links.forum.href.split("/").pop())) + 
+                                              "/" + JSON.stringify(parseInt(comment._links.subforum.href.split("/").pop())) +
+                                              "/" + comment.postId + 
+                                              "/" + comment.id + "/new"}>Reply</a>
+                                </ButtonGroup>
+                            </div>
+                        </Card.Body>
+        
+                    </Card>
+
+
+                        // <Card>
+                        //     <Card.Body>
+                        //         <Card.Title>Comment by {comment.username}</Card.Title>
+                        //         <Card.Subtitle>Comment Id: {comment.id} Post Id: {comment.postId}</Card.Subtitle>
+                        //         <Card.Text>{comment.commentContent}</Card.Text>
+                        //         {/*{console.log('PATH TO COMMENTS',start_url +*/}
+                        //         {/*    JSON.stringify(parseInt(post._links.forum.href.split("/").pop())) + '/' +*/}
+                        //         {/*    JSON.stringify(parseInt(post._links.subforum.href.split("/").pop())) + '/' +*/}
+                        //         {/*    JSON.stringify(parseInt(post._links.self.href.split("/").pop()*/}
+                        //         {/*    )))}*/}
+                        //         {/*<Card.Link href={start_url +*/}
+                        //         {/*JSON.stringify(parseInt(post._links.forum.href.split("/").pop())) + '/' +*/}
+                        //         {/*JSON.stringify(parseInt(post._links.subforum.href.split("/").pop())) + '/' +*/}
+                        //         {/*JSON.stringify(parseInt(post._links.self.href.split("/").pop()*/}
+                        //         {/*))}>Take me to the post {post.postId}</Card.Link>*/}
+                        //     </Card.Body>
+                        // </Card>
                     ))}
                 </Container>
 
